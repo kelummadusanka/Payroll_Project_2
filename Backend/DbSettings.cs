876b38e2666ea4c9b5d623bc67bc1f6b2b27375a
kelummadusanka <79236557+kelummadusanka@users.noreply.github.com>
2023-04-02 11:47:50 +0530

    final

--- Backend/DbSettings.cs
+++ Backend/DbSettings.cs
@@ -4,7 +4,6 @@ namespace Payroll_Project_2.Backend
 {
     public static class DbSettings
     {
-        //public static readonly string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=G:\Assigmnets\Payroll_Project_2\Payroll_Project_2\Database\Database.mdf;Integrated Security=True";
         public static readonly string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, "Database", "Database.mdf") + ";Integrated Security=True";
     }
 }
